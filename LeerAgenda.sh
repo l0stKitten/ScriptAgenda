12060b1d862d3c8c549b25f1f349fad13fccc8b1
dlopez <dlopezf@ulasalle.edu.pe>
2022-04-27 20:55:53 -0500

    Verificar si el archivo existe, sino crearlo

--- LeerAgenda.sh
+++ LeerAgenda.sh
@@ -1,1 +1,9 @@
 # Leer Agenda Script
+FILE=/home/d.lopez/SO/scripts/Agenda/asuntos.txt
+if [ -f "$FILE" ]; then
+	echo "$FILE existe"
+else 
+	echo "No exite el archivo"
+	touch asuntos.txt
+	echo "Se creó el archivo"
+fi
